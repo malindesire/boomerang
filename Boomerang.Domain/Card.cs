3b30822bc4b5e38053001c5eefee436f7612fd5a
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-12-04 19:05:33 +0100

    Set required and nullable in Card class

--- Boomerang.Domain/Card.cs
+++ Boomerang.Domain/Card.cs
@@ -2,11 +2,11 @@
 
 public class Card
 {
-	public string Name { get; set; }
-	public string Letter { get; set; }
-	public string Region { get; set; }
+	public required string Name { get; set; }
+	public required string Letter { get; set; }
+	public required string Region { get; set; }
 	public int Number { get; set; }
-	public string Collection { get; set; }
-	public string Animal { get; set; }
-	public string Activity { get; set; }
+	public string? Collection { get; set; }
+	public string? Animal { get; set; }
+	public string? Activity { get; set; }
 }
