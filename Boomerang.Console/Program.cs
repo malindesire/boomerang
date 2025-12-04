cd2f847c9c20dd98bd83edf35ab5dabf7f4dc21c
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-12-04 18:34:45 +0100

    Add loader and variant to Program

--- Boomerang.Console/Program.cs
+++ Boomerang.Console/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Boomerang.Infrastructure;
+using Boomerang.Application;
+
+var loader = new DeckLoader();
+var variant = new AustraliaVariant(loader);
+var deck = variant.LoadDeck();
+
+Console.WriteLine($"Loaded {deck.Count} cards for variant: {variant.Name}");
+
+foreach (var card in deck)
+{
+	Console.WriteLine($"- {card.Name}");
+}
